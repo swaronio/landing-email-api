3126846e16a8b962920612766853f7c727e4bb2f
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-04-14 17:38:54 -0300

    fix: update class variables after loading

--- configs/environment.py
+++ configs/environment.py
@@ -10,3 +10,11 @@ class Environment:
     @staticmethod
     def load_environment():
         dotenv.load_dotenv()
+        Environment.update_environment()
+        
+    def update_environment():
+        Environment.DB_USER = os.getenv('DB_USER')
+        Environment.DB_PASSWORD = os.getenv('DB_PASSWORD')
+        Environment.DB_HOST = os.getenv('DB_HOST')
+        Environment.DB_PORT = os.getenv('DB_PORT')
+        Environment.DB_NAME = os.getenv('DB_NAME')
