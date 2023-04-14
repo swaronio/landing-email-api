f5ff976813420640a6b8e23beab6145b67768dc6
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-04-14 15:12:00 -0300

    chore: setup environment file

---
+++ configs/environment.py
@@ -0,0 +1,11 @@
+import os, dotenv
+
+def load_environment():
+    dotenv.load_dotenv()
+
+class Environment:
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_HOST = os.getenv('DB_HOST')
+    DB_PORT = os.getenv('DB_PORT')
+    DB_NAME = os.getenv('DB_NAME')
