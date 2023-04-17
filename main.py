99e1355acbcfb5a5d5d4023fe23a95dddbb0024d
Pedro H <pedro_gonsalves@hotmail.com>
2023-04-17 19:42:04 -0300

    add: validation with regex and validation in the database

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
 from fastapi import FastAPI, HTTPException
-from sqlalchemy.orm import session
-from sqlalchemy import insert
 from database.connection import db
 import uvicorn
 from configs.environment import Environment
-from database.models.contact_schema import LandingEmail
+import re
 
 app = FastAPI()
 
 Environment.load_environment()
 db.connect()
-
-@app.get('/')
-def read_root():
-    size = len('pedro_gonsalves@hotmail.com')
-    return {"FODASE":size}
+db.create_table()
 
 @app.post('/email/{email}')
-async def emailInsert(email: str):
-    insert_res = db.exec_dml_com(
-        insert(LandingEmail).values(
-            email=email
-        )
-    )
-    db._session.add(insert_res)
-    db._session.commit()
+async def emailInsert(email:str):
+    pattern = r"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$"
+    validateEmail = re.match(pattern,email)
+    if validateEmail != None:
 
-    return {
-        "status":"SUCCESS",
-    }
+        query = db.exec_dql_comm(f"""
+                    select 1 as check from contact.landing_email where email = '{email}'
+        """)
+
+        if len(query.fetchall()) == 0:
+            db.exec_dml_com(f"""
+            insert into contact.landing_email (email) values ('{email}')
+            """)
+            return HTTPException(status_code=200,detail="Email entered successfully.")
+
+        else:
+            raise HTTPException(status_code=409, detail="Email already registered.")
+
+    else:
+        raise HTTPException(status_code=400, detail="Please enter a valid email.")
+
 
 if __name__ == '__main__':
     uvicorn.run(
