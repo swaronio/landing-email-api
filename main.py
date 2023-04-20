d2b5ce20cc3981955b79494b85a7149ef8b1e396
Pedro H <pedro_gonsalves@hotmail.com>
2023-04-20 14:18:29 -0300

    add: validations and remove __init__.py from models dir

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
-from typing import Annotated
-from fastapi import FastAPI, Form
+from fastapi import FastAPI,HTTPException, Body
 import uvicorn
+from sqlalchemy.exc import IntegrityError
 from database.connection import create_all, new_session
 from database.models.subscriber import Subscriber
+from email_validator import validate_email, EmailNotValidError, EmailSyntaxError
 
 app = FastAPI()
 
 create_all()
 
-
 @app.post('/register')
-async def register(email: Annotated[str, Form()]):
-    new_sub = Subscriber(email=email)
-    session = new_session()
-    session.add(new_sub)
-    session.commit()
-    return {"message": "You are now subscribed to swaron.io"}
-
+async def register(body = Body(...)):
+    email = body.get("email")
+    try:
+        emailInfo = validate_email(email, check_deliverability=False)
+        db = new_session()
+        subscriber = Subscriber(email=email)
+        db.add(subscriber)
+        db.commit()
+        return {"message": "Email registered successfully."}
+    except IntegrityError:
+        return HTTPException(status_code=409, detail="Email already registered.")
+    except EmailSyntaxError or EmailNotValidError:
+        return HTTPException(status_code=406, detail="Email format is not valid.",headers=None)
 
 if __name__ == '__main__':
     uvicorn.run(
