e50bc50648dc59b3f718bf0006c3f4143c2f4ac5
Maurício Antunes <mauricio.abreua@gmail.com>
2023-04-30 23:44:20 -0300

    Start using MySQL

--- configs/environment.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Environment:
-    DB_USER = os.environ['DB_USER']
-    DB_PASSWORD = os.environ['DB_PASSWORD']
-    DB_HOST = os.environ['DB_HOST']
-    DB_PORT = os.environ['DB_PORT']
-    DB_NAME = os.environ['DB_NAME']
