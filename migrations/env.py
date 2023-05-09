929ae18321bf3d4abd98757969b5f3d7ede1bcd7
Pedro H <pedro_gonsalves@hotmail.com>
2023-05-09 17:48:21 -0300

    fix: use new_engine function to perform migration

--- migrations/env.py
+++ migrations/env.py
@@ -1,6 +1,5 @@
 from logging.config import fileConfig
-import os
-from sqlalchemy import MetaData, create_engine
+from sqlalchemy import MetaData
 from alembic import context
 from database.connection import new_engine
 
@@ -13,18 +12,9 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# url connection
-db_user = os.environ.get("DB_USER")
-db_password = os.environ.get("DB_PASSWORD")
-db_host = os.environ.get("DB_HOST")
-db_name = os.environ.get("DB_NAME")
-
-
-db_url = f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}/{db_name}"
-
 
 def run_migrations_online() -> None:
-    connectable = create_engine(db_url)
+    connectable = new_engine()
 
     with connectable.connect() as connection:
         context.configure(connection=connection, target_metadata=metadata)
