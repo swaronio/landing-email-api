98d9b88c13ca54c0c5fce53ddee831e745a5eb9b
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-05-15 13:24:12 -0300

    revert email_sender

--- email_service/landing_page.py
+++ email_service/landing_page.py
@@ -4,7 +4,6 @@ from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from io import BytesIO
-from starlette.background import BackgroundTask
 
 import requests
 from PIL import Image
@@ -27,8 +26,7 @@ def register_user(email: str, db, email_sender):
 
     try:
         db.add(subscriber)
-        email_bg_task = BackgroundTask()
-        email_bg_task.add_task(email_sender, email)
+        email_sender(email)
     except:
         db.rollback()
         raise
